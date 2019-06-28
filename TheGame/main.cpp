3efe698e59281ecb2380bc24837ad2c6f9875006
Dmitry1q1 <49795221+Dmitry1q1@users.noreply.github.com>
2019-06-28 14:19:53 +0300

    фа

--- TheGame/main.cpp
+++ TheGame/main.cpp
@@ -5,31 +5,40 @@
 #include "view.h"
 using namespace sf;
 // размер персонажа
-const float h = 5;
-const float w = 5;
-
-void interactionWithMap(float x, float y, float dx, float dy){
-	Player player1(sf::Vector2f(150, 150), 100);//ф-ция взаимодействия с картой
-	for (int i = y / 90; i < (y + h) / 90; i++) {//проходимся по тайликам, контактирующим с игроком,, то есть по всем квадратикам размера 32*32, которые мы окрашивали в 9 уроке. про условия читайте ниже.
-		for (int j = x / 90; j < (x + w) / 90; j++){//икс делим на 32, тем самым получаем левый квадратик, с которым персонаж соприкасается. (он ведь больше размера 32*32, поэтому может одновременно стоять на нескольких квадратах). А j<(x + w) / 32 - условие ограничения координат по иксу. то есть координата самого правого квадрата, который соприкасается с персонажем. таким образом идем в цикле слева направо по иксу, проходя по от левого квадрата (соприкасающегося с героем), до правого квадрата (соприкасающегося с героем)
-			if (TileMap[i][j] == '0'){//если наш квадратик соответствует символу 0 (стена), то проверяем "направление скорости" персонажа:
+const float h = 40;
+const float w = 40;
+const int blcsize = 90;
+void interactionWithMap(Player& pp,float x, float y, float dx, float dy) {
+	//ф-ция взаимодействия с картой
+	/*float dx = pp.speed.x;
+	float dy = pp.speed.y;
+	sf::Vector2f v = pp.sprite.getPosition();
+	float x = v.x,startx=x;
+	float y = v.y,starty=y;*/
+	float startx = x;
+	float starty = y;
+	for (int i = y / blcsize; i < (y + h) / blcsize; i++) {
+		for (int j = x / blcsize; j < (x + w) / blcsize; j++){
+			if (TileMap[i][j] == '0'){
 				if (dy > 0){
-					y = i * 90 - h;
-					player1.setPosition(x, y);
+					y = i * blcsize - h;
+					pp.setPosition(startx, y);
 				}
 				if (dy < 0){
-					y = i * 90 + 90;
-					player1.setPosition(x, y);
+					y = i * blcsize + blcsize;
+					pp.setPosition(startx, y);
 				}
 				if (dx > 0){
-					x = j * 90 - w;
-					player1.setPosition(x, y);
+					x = j * blcsize - w;
+					pp.setPosition(x, starty);
 				}
 				if (dx < 0){
-					x = j * 90 + 90;
-					player1.setPosition(x, y);
+					x = j * blcsize + blcsize;
+					pp.setPosition(x, starty);
 				}
+				//pp.setPosition(x, y);
 			}
+			//pp.setPosition(x, y);
 			if (TileMap[i][j] == 'b') { //если символ равен 's' (камень)
 
 				TileMap[i][j] = ' ';//убираем камень, типа взяли бонус. можем и не убирать, кстати.
@@ -46,7 +55,7 @@ int main()
 
 	float CurrentFrame = 0;
 	sf::Clock clock;
-	Player player(sf::Vector2f(100, 100), 100);
+	Player player(sf::Vector2f(150, 150), 100);
 	Image map_image;
 	map_image.loadFromFile("assets/G_v01.png");
 	Texture map;
@@ -76,13 +85,13 @@ int main()
 		for (int i = 0; i < HEIGHT_MAP; i++) {
 			for (int j = 0; j < WIDTH_MAP; j++) {
 				if (TileMap[i][j] == ' ') {
-					s_map.setTextureRect(IntRect(0, 0, 90, 90));
+					s_map.setTextureRect(IntRect(360, 0, 90, 90));
 				}
 				if (TileMap[i][j] == 'b') {
 					s_map.setTextureRect(IntRect(465, 0, 90, 90));
 				}
 				if ((TileMap[i][j] == '0')) {
-					s_map.setTextureRect(IntRect(360, 0, 90, 90));
+					s_map.setTextureRect(IntRect(0, 0, 90, 90));
 				}
 				//s_map.scale(sf::Vector2f(0.112, 0.112));
 				s_map.setPosition(j * 90, i * 90);
@@ -94,12 +103,11 @@ int main()
 		float dx = player.speed.x;
 		float dy = player.speed.y;
 		v = player.sprite.getPosition();
-		interactionWithMap(v.x, v.y, dx, dy);
+		player.move(time);
+		interactionWithMap(player,v.x,v.y,dx,dy);
 		window.setView(view);
 
 		player.textureRotate(pos);
-		player.move(time);
-		//window.clear();
 		window.draw(player.sprite);
 		window.display();
 	}
