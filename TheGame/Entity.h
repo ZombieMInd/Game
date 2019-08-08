632949d3477695817ac07a9e61dc380326e118e7
Dmitry <mit.sosnilo@gmail.com>
2019-08-09 02:18:18 +0300

    с картой собачки не очень работают, закомментриуй функции вызова iter.. и все будет хорошо. Ну в смысле они ходить будут за тобой, но сквозь карту летают

--- TheGame/Entity.h
+++ TheGame/Entity.h
@@ -370,7 +370,7 @@ Enemy::Enemy(sf::Vector2f pos, int health) :
 void Enemy::update(float time, sf::Vector2f playerPos) {
 	behavior(playerPos);
 	move(5);
-	enemyInteractionWithMap(sprite.getPosition().x, sprite.getPosition().y, speed.x*time, speed.y*time);
+	//enemyInteractionWithMap(sprite.getPosition().x, sprite.getPosition().y, speed.x*5, speed.y*5);
 	textureRotate(playerPos);
 	sprite.setColor(sf::Color(255, 255, 255, 255));
 }
@@ -380,23 +380,15 @@ void Enemy::behavior(sf::Vector2f playerPos) {
 	if (distanceTo(playerPos) <= 700 && distanceTo(playerPos) > 400) {
 		if (time > 100) {
 			speed = getSpeed(playerPos);
-			//enemyInteractionWithMap(sprite.getPosition().x, sprite.getPosition().y, speed.x, speed.y);
-			/*std::cout << "distance " << distanceTo(playerPos) << std::endl;
-			std::cout << "speed " << speed.x << "  " << speed.y << std::endl;*/
+			enemyInteractionWithMap(sprite.getPosition().x, sprite.getPosition().y, speed.x, speed.y);
 			behaviorTimer.restart();
 		}
 	}
 	else if (distanceTo(playerPos) <= 400 && distanceTo(playerPos) > 120) {
-		//float time = behaviorTimer.restart().asMilliseconds();
 		speed = getSpeed(playerPos);
-		//enemyInteractionWithMap(sprite.getPosition().x, sprite.getPosition().y, speed.x, speed.y);
-			/*std::cout << "distance2222 " << distanceTo(playerPos) << std::endl;
-			std::cout << "time 2222" << time << std::endl;
-			std::cout << "speed " << speed.x << "  " << speed.y << std::endl;*/
-		//behaviorTimer.restart();
+		enemyInteractionWithMap(sprite.getPosition().x, sprite.getPosition().y, speed.x, speed.y);
 	}
 	else if (distanceTo(playerPos) <= 120) {	
-		//std::cout << "LASTD " << distanceTo(playerPos)  << std::endl;
 		speed.x = 0;
 		speed.y = 0;
 	}
@@ -405,8 +397,8 @@ void Enemy::behavior(sf::Vector2f playerPos) {
 void Enemy::enemyInteractionWithMap(float x, float y, float dx, float dy) {
 	float h, w;
 	bool chcoords = true;
-	h = getRealSize().y / 2;
-	w = getRealSize().x / 2;
+	w = getRealSize().y / 2;
+	h = getRealSize().x / 2;
 	y -= h;
 	x -= w;
 	float startX = x;
@@ -418,40 +410,44 @@ void Enemy::enemyInteractionWithMap(float x, float y, float dx, float dy) {
 		int j = dx <= 0 ? x / BLOCK_SIZE : (startX + 2 * w) / BLOCK_SIZE;
 		for (int i = y / BLOCK_SIZE; i < (startY + 2 * h) / BLOCK_SIZE; i++) {
 			if (TileMap[i][j] == '0') {
-				chcoords = false;
 				if (dx > 0 && TileMap[i][j - 1] != '0' && oldX <= j * BLOCK_SIZE - 2 * w) {
 					x = j * BLOCK_SIZE - 2 * w;
+					chcoords = false;
+					setPosition(x + w, y + h);
 					break;
 				}
 				if (dx < 0 && TileMap[i][j + 1] != '0' && oldX >= (j + 1) * BLOCK_SIZE) {
 					x = (j + 1) * BLOCK_SIZE;
+					chcoords = false;
+					setPosition(x + w, y + h);
 					break;
 				}
 			}
 		}
-		//setPosition(x, y);
 	}
 	if (dy != 0) {
 		int i = dy <= 0 ? y / BLOCK_SIZE : (startY + 2 * h) / BLOCK_SIZE;
 		for (int j = x / BLOCK_SIZE; j < (startX + 2 * w) / BLOCK_SIZE; j++) {
 			if (TileMap[i][j] == '0') {
-				chcoords = false;
 				if (dy > 0 && TileMap[i - 1][j] != '0' && oldY <= i * BLOCK_SIZE - 2 * h) {
 					y = i * BLOCK_SIZE - 2 * h;
+					chcoords = false;
+					setPosition(x + w, y + h);
 					break;
 				}
 				if (dy < 0 && TileMap[i + 1][j] != '0' && oldY >= (i + 1) * BLOCK_SIZE) {
 					y = (i + 1) * BLOCK_SIZE;
+					chcoords = false;
+					setPosition(x + w, y + h);
 					break;
 				}
 			}
 		}
-		//setPosition(x, y);
 	}
-	if (chcoords==false){
+	if (!chcoords){
 		speed.x = 0;
 		speed.y = 0;
-}
+	}
 }
 
 void Enemy::getDamage(int damage) {
